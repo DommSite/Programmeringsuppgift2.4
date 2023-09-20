3a7aa9cb92ed33b7675184faeb4c4e57f5725031
DommSite <johan.johansson06@live.se>
2023-09-20 10:13:19 +0200

    Add project files.

---
+++ Programmeringsuppgift2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace övning2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket tjänar person 1? (kr/h)");
+            float lön1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket tjänar person 2? (kr/h)");
+            float lön2 = float.Parse(Console.ReadLine()); 
+            Console.WriteLine("Hur mycket tjänar person 3? (kr/h)");
+            float lön3 = float.Parse(Console.ReadLine());
+            float medellön = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medellönen för företagets personal är " + medellön + "kr per timme");
+            Console.ReadKey();
+        }
+    }
+}
